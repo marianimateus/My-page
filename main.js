76f8ac9cf5f8612bbb4a6f5be22560009ca0d7b0
Mateus Mariani <mzmariani72@gmail.com>
2024-07-16 19:19:08 -0300

    customizing the website

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var container = document.querySelector(".container");
+    if (container) {
+        container.style.boxShadow = "0px 0px 25px 10px rgba(0, 0, 0, 0.4)";
+    }
+});
